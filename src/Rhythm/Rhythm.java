d3ecdde6e3774c88cb61a102868d087dfdc0b39a
sashakoushk-jalali <skoushkjalali@gmail.com>
2022-06-21 11:01:52 +0100

    Rhythm class and tests done

---
+++ src/Rhythm/Rhythm.java
@@ -0,0 +1,60 @@
+package Rhythm;
+
+import java.util.Arrays;
+
+/*
+    This class represents a rhythm in its relative form, as an int array of onsets, and the corresponding number of
+    segments in the rhythm.
+    segments = rhythmData[0] is the number of equal time segments in the rhythm.
+ */
+public class Rhythm {
+    private final int[] onsets;
+    private final int segments;
+
+    Rhythm(int[] rhythmData){
+        this.segments = rhythmData[0];
+        this.onsets = Arrays.copyOfRange(rhythmData,1, rhythmData.length);
+    }
+
+    public int getSegments() {
+        return segments;
+    }
+
+    public int[] getRelativeRhythm() {
+        return onsets;
+    }
+
+    /*
+        This method returns a double array of the bpm scaled, absolute onset location of the relative rhythm held in
+        onsets.
+        Each value is the number of milliseconds from 0 that the onset occurs, at the bpm that is passed as @param bpm.
+     */
+    public double[] getAbsoluteRhythm(double bpm){
+
+        // total duration of rhythm in milliseconds
+        double totalDurationOfRhythm = (4 / bpm) * 60 * 1000;
+
+        // total duration of each bit (or segment) in milliseconds
+        double durationOfEachBit = totalDurationOfRhythm / this.segments;
+
+        // array holds the temporal location of each onset from a zero point in time.
+        return Arrays.stream(onsets).mapToDouble(o -> o*durationOfEachBit).toArray();
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
