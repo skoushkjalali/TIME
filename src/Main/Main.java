fd8751b372aee34b127c6fa60c7f2443e5ea5c3f
sashakoushk-jalali <skoushkjalali@gmail.com>
2022-07-09 16:52:42 +0100

    fixed unknown off-by-one bug in rhythm data. Added 5 more rhythms. Also altered Main.main to run all rhythms consecutively, which makes user testing easier.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -14,30 +14,32 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        // select a rhythm
 
-        Rhythm r = RhythmFactory.getRhythm(3);
+        for(int i = 1; i<=25;i++) {
+            // run through all 25 rhythms
+            Rhythm r = RhythmFactory.getRhythm(i);
 
-        // pass rhythm, bpm, scoring bounds into gameDriver
-        Driver gameDriver = new Driver(100, 30, 800, r);
+            // pass rhythm, bpm, scoring bounds into gameDriver
+            Driver gameDriver = new Driver(90, 40, 750, r);
 
-        // set up the player, listener and scorer,
-        RhythmPlayer rhythmPlayer = new RhythmPlayer(gameDriver.getBpm());
-        RhythmListener listener = new RhythmListener();
-        Scorer scorer = new Scorer(gameDriver.getLOWER_BOUND(), gameDriver.getUPPER_BOUND());
+            // set up the player, listener and scorer,
+            RhythmPlayer rhythmPlayer = new RhythmPlayer(gameDriver.getBpm());
+            RhythmListener listener = new RhythmListener();
+            Scorer scorer = new Scorer(gameDriver.getLOWER_BOUND(), gameDriver.getUPPER_BOUND());
 
-        // initialise keyboard listening
-        listener.userTaps();
+            // initialise keyboard listening
+            listener.userTaps();
 
-        // play 4 clicks of metronome
-        Metronome.playMetronome(gameDriver.getBpm());
+            // play 4 clicks of metronome
+            Metronome.playMetronome(gameDriver.getBpm());
 
-        // play the selected rhythm
-        rhythmPlayer.playRhythm(r);
+            // play the selected rhythm
+            rhythmPlayer.playRhythm(r);
 
-        // wait to get userInput from keyboard, including waiting for enough time for the input
-        Thread.sleep(gameDriver.getBarDurationInMilliSecs()+1000);
-        double score = scorer.scoreInput(r.getAbsoluteRhythm(gameDriver.getBpm()), listener.getShiftedUserInput(gameDriver.getBpm(), 2));
-        System.out.println(score);
+            // wait to get userInput from keyboard, including waiting for enough time for the input
+            Thread.sleep(gameDriver.getBarDurationInMilliSecs() + 1000);
+            double score = scorer.scoreInput(r.getAbsoluteRhythm(gameDriver.getBpm()), listener.getShiftedUserInput(gameDriver.getBpm(), 2));
+            System.out.println("Rhythm "+i +": "+ (int)(score*100) + "%");
+        }
     }
 }
--- src/Rhythm/RhythmFactory.java
+++ src/Rhythm/RhythmFactory.java
@@ -1,6 +1,7 @@
 package Rhythm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
     This is a mock class to allow RhythmListener and RhythmPlayer to bew built and tested.
@@ -25,15 +26,61 @@ public class RhythmFactory {
     // Complex Rhythm Example 1
     static int[] rhythmData4 = {96,0,24,30,36,42,48,56,64,72,84,87,90};
 
+    static int[][] rhythmData =
+            {       // crotchets and quavers
+                    // each beat = 2 segments
+                    {8,0,2,3,4,6},
+                    {8,0,1,2,4,5,6},
+                    {8,0,1,3,4,6},
+                    {8,0,3,4,5,7},
+                    {8,2,3,5,7},
+                    {8,1,3,5,6},
 
-    public static Rhythm getRhythm(int rhythmNumber){
+                    // add semiquavers
+                    // each beat = 4 segments
+                    {16,0,4,7,8,12,14},
+                    {16,0,2,4,5,6,7,8,11,12},
+                    {16,0,2,3,4,6,8,9,10,11,12,15},
+                    {16,2,3,4,7,10,12,13},
+                    {16,1,4,6,7,8,9,11,14},
+                    {16,1,3,6,7,8,11,13},
 
-        switch (rhythmNumber){
-            case 0 -> {return new Rhythm(rhythmData0);}
-            case 1 -> {return new Rhythm(rhythmData1);}
-            case 2 -> {return new Rhythm(rhythmData2);}
-            case 3 -> {return new Rhythm(rhythmData3);}
-            default -> {return new Rhythm(rhythmData4);}
+                    // add triplets
+                    {48,0,6,12,16,20,24,33,36},
+                    {48,0,3,9,12,18,21,24,28,32,42},
+                    {48,0,9,12,18,21,24,28,36,40,44},
+                    {48,5,12,15,18,21,24,28,32,40},
+                    {48,0,12,18,24,28,32,36,39,42,45},
+                    {48,0,6,12,14,16,18,24,28,36,39},
+
+                    // add quintuplets
+                    {240,0,30,60,80,100,120,135,150,165,180,192,204,216,228},
+                    {240,0,30,45,60,84,108,132,156,180,200,220},
+                    {240,0,10,20,30,60,80,100,165,192,204,216,228},
+                    {5,0,1,2,3,4},
+                    {240,0,12,36,48,72,84,108,120,150,165,200,220},
+                    {240,0,30,90,100,110,120,140,150,160,180,210,216,222,228,234},
+                    {240,0,15,30,40,50,80,100,110,120,144,168,180,192,216,228}
+            };
+
+    public static Rhythm getRhythm(int n){
+        if(n<0 || n > rhythmData.length){
+            n = 1;
         }
+        return new Rhythm(rhythmData[n-1]);
     }
+
+//    public static void main(String[] args) {
+//        int[][] newRhythmData = rhythmData.clone();
+//        for(int i = 0; i<rhythmData.length; i++){
+//            for(int j = 2; j<rhythmData[i].length; j++){
+//                newRhythmData[i][j] += 1;
+//            }
+//        }
+//        System.out.println(Arrays.deepToString(newRhythmData));
+//    }
+
+
+
 }
+
