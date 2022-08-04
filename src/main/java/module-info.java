d6336f6b57316f61d604c75bc38b198203b67914
sashakoushk-jalali <skoushkjalali@gmail.com>
2022-08-04 13:44:21 +0100

    Updated module-info to export UserProfile

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,27 +7,26 @@ module time {
 
 
     opens com.time.game to javafx.fxml;
+    opens com.time.game.Controller.GamePlay to javafx.fxml;
+    opens com.time.game.Controller.Profile to javafx.fxml;
+    opens com.time.game.Controller to javafx.fxml;
+    opens com.time.game.Controller.EndOfLevel to javafx.fxml;
+    opens com.time.game.Controller.Login to javafx.fxml;
+    exports com.time.game.Controller to javafx.fxml;
+    exports com.time.game.Controller.Login to javafx.fxml;
+
     exports com.time.game;
     exports com.time.game.Model.Rhythm;
-
-    opens com.time.game.Controller.GamePlay to javafx.fxml;
     exports com.time.game.Controller.GamePlay;
-
-
-    opens com.time.game.Controller.Profile to javafx.fxml;
     exports com.time.game.Controller.Profile;
-
-    opens com.time.game.Controller to javafx.fxml;
-    exports com.time.game.Controller to javafx.fxml;
     exports com.time.game.GameLogic.Rhythm;
-
     exports com.time.game.Model.Level;
     exports com.time.game.GameLogic.Scorer;
     exports com.time.game.Controller.EndOfLevel;
-    opens com.time.game.Controller.EndOfLevel to javafx.fxml;
+    exports com.time.game.Model.Profile;
 
-    exports com.time.game.Controller.Login to javafx.fxml;
-    opens com.time.game.Controller.Login to javafx.fxml;
+
+
 
 
 }
