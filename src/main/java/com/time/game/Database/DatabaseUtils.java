d8797e51558608598d172db8cacbcb5670e1b5d0
sashakoushk-jalali <skoushkjalali@gmail.com>
2022-08-09 13:46:17 +0100

    Added database package, included tests and implementations for processing score data to and from the database

---
+++ src/main/java/com/time/game/Database/DatabaseUtils.java
@@ -0,0 +1,59 @@
+package com.time.game.Database;
+
+import java.util.ArrayList;
+
+public class DatabaseUtils {
+
+
+    public static boolean validateExistingUser(String username, String password){
+
+        return true;
+    }
+
+
+    /*
+        Method converts integer values held in an arraylist to a single string. The string comprises a string of codes.
+        A digit corresponding to the number of digits in the next score, and then the score. This pattern is repeated.
+        i.e., 18 = 1 digit, score = 8; 210 = 2 digits, score = 10; 3100 = 3 digits, score = 100.
+        This would result in a string of "18210103100"
+     */
+    public static String compressUserLevelScores(ArrayList<Integer> userLevelScores){
+        if(userLevelScores == null){
+            return "";
+        }
+        StringBuilder compressedScores = new StringBuilder();
+        String scoreStr;
+        int length;
+
+        for(int score : userLevelScores){
+            scoreStr = Integer.toString(score);
+            length = scoreStr.length();
+            compressedScores.append(length).append(scoreStr);
+        }
+        return compressedScores.toString();
+    }
+
+    /*
+        Method converts a string of integer scores to an arrayList of those scores.
+        The string uses the same compression method as above.
+        "10" = 1 digit, score = 0; "283" = 2 digits, score = 83.
+        Therefore, a string of "10256310016" = ArrayList of {0, 56, 100, 6}
+     */
+    public static ArrayList<Integer> decompressUserLevelScores(String compressedUserLevelScores){
+        ArrayList<Integer> userLevelScores = new ArrayList<>();
+        int numberOfDigits;
+        int score;
+        String scoreStr = "";
+        for(int i = 0; i < compressedUserLevelScores.length();){
+            numberOfDigits = Integer.parseInt(Character.toString(compressedUserLevelScores.charAt(i)));
+            for(int j = 1; j <= numberOfDigits; j++) {
+                scoreStr += compressedUserLevelScores.charAt(i + j);
+            }
+            score = Integer.parseInt(scoreStr);
+            userLevelScores.add(score);
+            scoreStr = "";
+            i+= numberOfDigits + 1;
+        }
+        return userLevelScores;
+    }
+}
---
+++ src/test/java/com/time/game/Database/DatabaseTest.java
@@ -0,0 +1,68 @@
+package com.time.game.Database;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+
+
+    @Test
+    void testCompressUserLevelScores(){
+        ArrayList<Integer> userScores = new ArrayList<>(List.of(34,74,0,100,6));
+        String result = DatabaseUtils.compressUserLevelScores(userScores);
+        assertEquals("23427410310016", result);
+    }
+
+    @Test
+    void testCompressUserLevelScores1(){
+        ArrayList<Integer> userScores = null;
+        String result = DatabaseUtils.compressUserLevelScores(userScores);
+        assertEquals("", result);
+    }
+
+    @Test
+    void testCompressUserLevelScores2(){
+        ArrayList<Integer> userScores = new ArrayList<>(List.of(0));
+        String result = DatabaseUtils.compressUserLevelScores(userScores);
+        assertEquals("10", result);
+    }
+
+    @Test
+    void testDecompressUserLevelScores3(){
+        ArrayList<Integer> result = DatabaseUtils.decompressUserLevelScores("23427410310016");
+        ArrayList<Integer> expected = new ArrayList<>(List.of(34,74,0,100,6));
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void testDecompressUserLevelScores4(){
+        ArrayList<Integer> result = DatabaseUtils.decompressUserLevelScores("");
+        ArrayList<Integer> expected = new ArrayList<>();
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void testDecompressUserLevelScores5(){
+        ArrayList<Integer> result = DatabaseUtils.decompressUserLevelScores("11255310010");
+        ArrayList<Integer> expected = new ArrayList<>(List.of(1,55,100,0));
+        assertEquals(expected,result);
+    }
+
+
+    @Test
+    void testValidateExistingUserTrue(){
+        assertTrue(DatabaseUtils.validateExistingUser("SashaKJ", "admin"));
+    }
+
+    @Test
+    void testValidateExistingUserFalse(){
+        assertTrue(DatabaseUtils.validateExistingUser("Simon", "Kablamo"));
+    }
+
+
+}
