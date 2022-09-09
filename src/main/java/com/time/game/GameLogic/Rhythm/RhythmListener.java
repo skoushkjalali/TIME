3c68105d620c0eaa5ec9d4b461bcf5c18ba03e62
sashakoushk-jalali <skoushkjalali@gmail.com>
2022-09-09 22:54:03 +0100

    Changed error in comment in RhythmListener to say milliseconds not bars

--- src/main/java/com/time/game/GameLogic/Rhythm/RhythmListener.java
+++ src/main/java/com/time/game/GameLogic/Rhythm/RhythmListener.java
@@ -23,11 +23,11 @@ public class RhythmListener {
 
     /*
                 This method shifts the user tap input information forward by the number of bars specified using
-                @param bars at the speed of @param bpm. This allows RhythmListener to be setup to listen to key input before
-                the first beat of the required bar.
+                @param bars at the speed of @param bpm.
              */
     public ArrayList<Integer> getShiftedUserInput(double bpm, double bars){
-        // number of bars to shift the start of the bar to
+
+        // number of milliseconds to shift each userTap
         int shift = (int) ((60 / bpm) * 1000 * 4 * bars);
 
         ArrayList<Integer> shiftedInput = new ArrayList<>();
