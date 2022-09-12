482a4df3aaafc819ac2819416715be206b0f096c
sashakoushk-jalali <skoushkjalali@gmail.com>
2022-09-12 11:37:32 +0100

    Refactored Level and Level test for clarity

--- src/main/java/com/time/game/Model/Level/Level.java
+++ src/main/java/com/time/game/Model/Level/Level.java
@@ -7,69 +7,68 @@ import com.time.game.Model.Rhythm.Rhythm;
     This class represents a level of a rhythm at a prescribed bpm.
  */
 
-    public class Level {
-        private boolean userInputCaptureEnabled = false;
-        private static int bpm = 100;
-        private final int barDurationInMilliSecs = (int)((60 / (float)bpm) * 1000 * 4);
-        final int LOWER_BOUND = 20;
-        final int UPPER_BOUND = 250;
-        private static int levelNumber = 1;
-        private static int lastScore = 0;
-        private final Rhythm sampleRhythm;
+public class Level {
+    private boolean userInputCaptureEnabled = false;
+    private static int bpm = 100;
+    final int LOWER_BOUND = 20;
+    final int UPPER_BOUND = 250;
+    private static int levelNumber = 1;
+    private static int lastScore = 0;
+    private final Rhythm sampleRhythm;
 
-        public Level(Rhythm sampleRhythm){
-            this.sampleRhythm = sampleRhythm;
-        }
-
-        public static int getLastScore(){
-            return Level.lastScore;
-        }
-
-        public static void setLastScore(int score){
-            Level.lastScore = score;
-        }
-
-        public static int getLevelNumber() {
-            return levelNumber;
-        }
-
-        public static void setLevelNumber(int levelNumber) {
-            Level.levelNumber = levelNumber;
-        }
-
-        public Rhythm getSampleRhythm() {
-            return sampleRhythm;
-        }
-
-        public static int getBpm() {
-            return bpm;
-        }
-
-        public static void setBpm(int bpm){
-            Level.bpm = bpm;
-        }
-
-        public int getBarDurationInMilliSecs() {
-            return barDurationInMilliSecs;
-        }
-
-        public int getBeatDurationInMilliSecs(){
-            return barDurationInMilliSecs / 4;
-        }
-
-        public int getLOWER_BOUND() {
-            return LOWER_BOUND;
-        }
-
-        public int getUPPER_BOUND() {
-            return UPPER_BOUND;
-        }
-
-        public boolean isUserInputCaptureEnabled() {
-            return userInputCaptureEnabled;
-        }
-
-        public void setUserInputCaptureEnabled(boolean status) {
-            userInputCaptureEnabled = status;
-        }
+    public Level(Rhythm sampleRhythm){
+        this.sampleRhythm = sampleRhythm;
     }
+
+    public static int getLastScore(){
+        return Level.lastScore;
+    }
+
+    public static void setLastScore(int score){
+        Level.lastScore = score;
+    }
+
+    public static int getLevelNumber() {
+        return levelNumber;
+    }
+
+    public static void setLevelNumber(int levelNumber) {
+        Level.levelNumber = levelNumber;
+    }
+
+    public Rhythm getSampleRhythm() {
+        return sampleRhythm;
+    }
+
+    public static int getBpm() {
+        return bpm;
+    }
+
+    public static void setBpm(int bpm){
+        Level.bpm = bpm;
+    }
+
+    public int getBarDurationInMilliSecs() {
+        return (int) ( (60 / (float) bpm) * 1000 * 4);
+    }
+
+    public int getBeatDurationInMilliSecs(){
+        return getBarDurationInMilliSecs() / 4;
+    }
+
+    public int getLOWER_BOUND() {
+        return LOWER_BOUND;
+    }
+
+    public int getUPPER_BOUND() {
+        return UPPER_BOUND;
+    }
+
+    public boolean isUserInputCaptureEnabled() {
+        return userInputCaptureEnabled;
+    }
+
+    public void setUserInputCaptureEnabled(boolean status) {
+        userInputCaptureEnabled = status;
+    }
+}
--- src/test/java/LevelTest.java
+++ src/test/java/LevelTest.java
@@ -8,30 +8,181 @@ import static org.junit.jupiter.api.Assertions.*;
 class LevelTest {
     Rhythm r = RhythmFactory.getRhythm(10);
     Level level = new Level(r);
-    int order = 1;
+    int order = 3;
+
+
 
     @Test
-    void testGetBarDurationInMilliSecs() {
-        if (order == 1) {
-            Level.setBpm(100);
-            int duration = level.getBarDurationInMilliSecs();
-            assertEquals(duration, 2400);
-        }
-        order+=1;
+    void testGetBarDurationInMilliSecs10() {
+        Level.setBpm(10);
+        assertEquals(24000, level.getBarDurationInMilliSecs());
     }
 
     @Test
-    void testGetBeatDurationInMilliSecs() {
-        if (order == 2) {
-            Level.setBpm(100);
-            int duration = level.getBeatDurationInMilliSecs();
-            assertEquals(duration, 600);
-        }
-        order +=1;
+    void testGetBeatDurationInMilliSecs10() {
+        Level.setBpm(10);
+        assertEquals(6000, level.getBeatDurationInMilliSecs());
     }
 
     @Test
-    void testgetLastScore() {
+    void testGetBarDurationInMilliSecs20() {
+        Level.setBpm(20);
+        assertEquals(12000, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBeatDurationInMilliSecs20() {
+        Level.setBpm(20);
+        assertEquals(3000, level.getBeatDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs30() {
+        Level.setBpm(30);
+        assertEquals(8000, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBeatDurationInMilliSecs30() {
+        Level.setBpm(30);
+        assertEquals(2000, level.getBeatDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs40() {
+        Level.setBpm(40);
+        assertEquals(6000, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBeatDurationInMilliSecs40() {
+        Level.setBpm(40);
+        assertEquals(1500, level.getBeatDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs50() {
+        Level.setBpm(50);
+        assertEquals(4800, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBeatDurationInMilliSecs50() {
+        Level.setBpm(50);
+        assertEquals(1200, level.getBeatDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs60() {
+        Level.setBpm(60);
+        assertEquals(4000, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBeatDurationInMilliSecs60() {
+        Level.setBpm(60);
+        assertEquals(1000, level.getBeatDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs70() {
+        Level.setBpm(70);
+        assertEquals(3428, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBeatDurationInMilliSecs70() {
+        Level.setBpm(70);
+        assertEquals(857, level.getBeatDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs80() {
+        Level.setBpm(80);
+        assertEquals(3000, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBeatDurationInMilliSecs80() {
+        Level.setBpm(80);
+        assertEquals(750, level.getBeatDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs90() {
+        Level.setBpm(90);
+        assertEquals(2666, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs100() {
+        Level.setBpm(100);
+        assertEquals(2400, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs110() {
+        Level.setBpm(110);
+        assertEquals(2181, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs120() {
+        Level.setBpm(120);
+        assertEquals(2000, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs130() {
+        Level.setBpm(130);
+        assertEquals(1846, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs140() {
+        Level.setBpm(140);
+        assertEquals(1714, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs150() {
+        Level.setBpm(150);
+        assertEquals(1600, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs160() {
+        Level.setBpm(160);
+        assertEquals(1500, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs170() {
+        Level.setBpm(170);
+        assertEquals(1411, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs180() {
+        Level.setBpm(180);
+        assertEquals(1333, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs190() {
+        Level.setBpm(190);
+        assertEquals(1263, level.getBarDurationInMilliSecs());
+    }
+
+    @Test
+    void testGetBarDurationInMilliSecs200() {
+        Level.setBpm(200);
+        assertEquals(1200, level.getBarDurationInMilliSecs());
+    }
+
+
+    @Test
+    void testGetLastScore() {
         if (order == 3) {
             assertEquals(0, Level.getLastScore());
         }
@@ -84,7 +235,6 @@ class LevelTest {
     void testGetLOWER_BOUND(){
         if (order == 9) {
             assertEquals(20, level.getLOWER_BOUND());
-            assertEquals(250, level.getUPPER_BOUND());
         }
         order +=1;
     }
@@ -92,7 +242,6 @@ class LevelTest {
     @Test
     void testGetUPPER_BOUND(){
         if (order == 10) {
-            assertEquals(20, level.getLOWER_BOUND());
             assertEquals(250, level.getUPPER_BOUND());
         }
         order +=1;
