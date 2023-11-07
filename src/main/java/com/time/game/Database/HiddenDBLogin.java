1c04bcb85e3ad628db5738638cc36218c6574a93
sashakoushk-jalali <skoushkjalali@gmail.com>
2023-11-07 18:25:05 +0000

    Added DB login as repo changed to private

---
+++ src/main/java/com/time/game/Database/HiddenDBLogin.java
@@ -0,0 +1,13 @@
+package com.time.game.Database;
+
+public class HiddenDBLogin {
+
+    static String getDBUsername(){
+        return "admin";
+    }
+
+    static String getDBPassword(){
+        return "TimeApplication1987";
+    }
+
+}
